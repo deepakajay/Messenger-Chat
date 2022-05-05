0eabf5022ac7d4d1cfada2638943a0ccfc3b9335
deepak <deepakajay77@gmail.com>
2022-05-05 17:36:37 +0530

    chat page header added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react"
+
+import axios from 'axios'
+import { useHistory } from "react-router-dom"
+import { ChatEngine } from 'react-chat-engine'
+
+import { useAuth } from "../contexts/AuthContext"
+
+import { auth } from "../firebase"
 
 function Chats() {
   return (
-    <div>Chats</div>
+    <div className='chats-page'>
+    <div className='nav-bar'>
+      <div className='logo-tab'>
+        Dechat
+      </div>
+
+      <div className='logout-tab'>
+        Logout
+      </div>
+    </div>
+  </div>
   )
 }
 
